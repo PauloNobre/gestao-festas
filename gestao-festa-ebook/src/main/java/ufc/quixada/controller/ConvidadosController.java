76b7112ed656d46637275dfcd8defa0eb9d7d40c
PauloHenrique <PauloHenrique@PauloNobre>
2016-08-01 15:34:47 -0300

    listando todos os convidados na tela

--- gestao-festa-ebook/src/main/java/ufc/quixada/controller/ConvidadosController.java
+++ gestao-festa-ebook/src/main/java/ufc/quixada/controller/ConvidadosController.java
@@ -1,13 +1,22 @@
 package ufc.quixada.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ufc.quixada.repository.ConvidadoRepository;
 
 @Controller
 public class ConvidadosController {
+	
+	@Autowired
+	ConvidadoRepository convidadoRepository;
 
 	@RequestMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("ListaConvidados");
+		mv.addObject("convidados", convidadoRepository.todos());
+		return mv;
 	}
 }
