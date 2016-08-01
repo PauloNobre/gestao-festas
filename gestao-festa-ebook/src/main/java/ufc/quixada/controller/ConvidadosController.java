d64c4608971463405329daa507e7bb8907d97f98
PauloHenrique <PauloHenrique@PauloNobre>
2016-08-01 16:52:47 -0300

    adicionar novo convidado

--- gestao-festa-ebook/src/main/java/ufc/quixada/controller/ConvidadosController.java
+++ gestao-festa-ebook/src/main/java/ufc/quixada/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package ufc.quixada.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ufc.quixada.model.Convidado;
 import ufc.quixada.repository.ConvidadoRepository;
 
 @Controller
@@ -17,6 +19,13 @@ public class ConvidadosController {
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("ListaConvidados");
 		mv.addObject("convidados", convidadoRepository.todos());
+		mv.addObject(new Convidado());
 		return mv;
+	}
+	
+	@RequestMapping(value="/cadastrar", method = RequestMethod.POST)
+	public String cadastrar(Convidado convidado) {
+		this.convidadoRepository.adicionar(convidado);
+		return "redirect:/convidados";
 	}
 }
