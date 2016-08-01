4fa14851e703625637b031d66be9c13690a4895c
PauloHenrique <PauloHenrique@PauloNobre>
2016-08-01 14:34:53 -0300

    criaçao do projeto

---
+++ gestao-festa-ebook/src/main/java/ufc/quixada/GestaoFestaEbookApplication.java
@@ -0,0 +1,12 @@
+package ufc.quixada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFestaEbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFestaEbookApplication.class, args);
+	}
+}
